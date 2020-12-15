ee2e85bb8b1e59905fdf6f71b91582a0d64b973d
Uh yeh no <uhyehno@gmail.com>
2020-12-14 23:14:07 -0300

    actual website and clean up

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders worq in progress text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const p = screen.getByText(/This website is a worq in progress./i);
+  expect(p).toBeInTheDocument();
 });
